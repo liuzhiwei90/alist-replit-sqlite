33b3bcf93e6eff6a74be3dfe67d0de6fb4e22c31
溜追 <86212054+liuzhiwei90@users.noreply.github.com>
2023-02-18 12:08:42 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@ if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-fi
-./alist server --no-prefix
-
-if [ ! -f "data.db" ];then
+else
+  if [ ! -f "data.db" ];then
   cp data.db ../data.db
   rm -f data.db
   sed -i "s#data\\/data.db#..\\/data\\/data.db#g" ./data/config.json
+  fi
 fi
+./alist server --no-prefix
